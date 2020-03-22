a5c7f2064104f92b4f348710c49aabc01111dcfb
Sergio <Sergio@DESKTOP-447NUQP.localdomain>
2020-03-22 18:39:00 +0100

    Commit del resultado final de MainClass

--- proyectoContarPalabras/src/proyectocontarpalabras/MainClass.java
+++ proyectoContarPalabras/src/proyectocontarpalabras/MainClass.java
@@ -7,52 +7,27 @@ import java.util.Scanner;
  */
 public class MainClass {
 
-    //PROPIEDADES 
+ 
     static Scanner sc = new Scanner(System.in);
     static int iTotalCaracteres=0,iTotalPalabras=0;
     static String sFrase;
 
     
-     //METODOS Mostrar
-    
-     static String textoCaracteresTotal (int total) {
-	   
-	   String sResultado="El número total de caracteres es "+total;
-	   
-	   return sResultado;
-   }
-      
-     static String textoNumeroPalabras (int total) {
-	   String sResultado="El número total de palabras es "+total;
-	   
-	   return sResultado;
-   }
-      
-     static String textoTodosCaracteres (int a[]) {
-	   
-	   String sResultado="";
-	   
-	       for (int i=0;i<aiVecesCaracter.length;i++)
-	    	   if(aiVecesCaracter[i]!=0)
-	    		   sResultado+="Caracter "+ acAbecedario[i]+": "+a[i]+" veces.\n";
-	       
-	   return sResultado;
-	   
-   }
+
    
     public static void main (String [] args) {
 
         System.out.println("Escribe una frase");
         sFrase = sc.nextLine();
         int iTotalCaracteres1;
-        iTotalCaracteres1 = contarCaracteresTotal(sFrase);
-        iTotalPalabras = contarNumeroPalabras(sFrase);
-        aiVecesCaracter= contarTodosCaracteres(sFrase);
+        iTotalCaracteres1 = ContarPalabras.contarCaracteresTotal(sFrase);
+        iTotalPalabras = ContarPalabras.contarNumeroPalabras(sFrase);
+        ContarPalabras.aiVecesCaracter= ContarPalabras.contarTodosCaracteres(sFrase);
         
         
-        System.out.println(textoCaracteresTotal (iTotalCaracteres));
-        System.out.println(textoNumeroPalabras (iTotalPalabras));
-        System.out.println(textoTodosCaracteres(aiVecesCaracter));
+        System.out.println(MostrarPalabras.textoCaracteresTotal (iTotalCaracteres));
+        System.out.println(MostrarPalabras.textoNumeroPalabras (iTotalPalabras));
+        System.out.println(MostrarPalabras.textoTodosCaracteres(ContarPalabras.aiVecesCaracter));
 
         
     }//Main
