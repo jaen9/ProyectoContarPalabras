0d3bf18f775477a1147e5f752d5fabb17c3e1f49
Sergio <Sergio@DESKTOP-447NUQP.localdomain>
2020-03-22 18:27:53 +0100

    He creado la primera clase de MostrarPalabras eliminando su código de MainClass

--- proyectoContarPalabras/src/proyectocontarpalabras/MostrarPalabras.java
+++ proyectoContarPalabras/src/proyectocontarpalabras/MostrarPalabras.java
@@ -1,9 +1,33 @@
 
 package proyectocontarpalabras;
-/**
- * 
- * @author Sergio
- */
+
+
 public class MostrarPalabras {
     
+       
+       
+         static String textoCaracteresTotal (int total) {
+	   
+	   String sResultado="El número total de caracteres es "+total;
+	   
+	   return sResultado;
+   }
+      
+     static String textoNumeroPalabras (int total) {
+	   String sResultado="El número total de palabras es "+total;
+	   
+	   return sResultado;
+   }
+      
+     static String textoTodosCaracteres (int a[]) {
+	   
+	   String sResultado="";
+	   
+	       for (int i=0;i<ContarPalabras.aiVecesCaracter.length;i++)
+	    	   if(ContarPalabras.aiVecesCaracter[i]!=0)
+	    		   sResultado+="Caracter "+ ContarPalabras.acAbecedario[i]+": "+a[i]+" veces.\n";
+	       
+	   return sResultado;
+	   
+   }
 }
